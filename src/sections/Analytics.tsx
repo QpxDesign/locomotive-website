a79134af1d337f34df7fd1385885466f028f5d1a
Quinn555 <71609644+Quinn555@users.noreply.github.com>
2024-02-01 11:52:05 -0500

    added stats

--- src/sections/Analytics.tsx
+++ src/sections/Analytics.tsx
@@ -7,11 +7,11 @@ export default function Analytics() {
       <h1 style={{ padding: ".5em" }}>Our Results</h1>
       <div className="stats-grid full-center">
         <Ticker
-          headlineNumber={14000}
+          headlineNumber={22000}
           subtext={"Downloads on Apple App Store"}
         />
-        <Ticker headlineNumber={10000} subtext={"Monthly Active Users"} />
-        <Ticker headlineNumber={20} subtext={"Five Star Reviews"} />
+        <Ticker headlineNumber={15000} subtext={"Monthly Active Users"} />
+        <Ticker headlineNumber={30} subtext={"Five Star Reviews"} />
         <Ticker headlineNumber={300} subtext={"Train Trips/Day"} />
         <Ticker headlineNumber={15} subtext={"Custom Graphics"} />
         <Ticker headlineNumber={50} subtext={"States With Users"} />
